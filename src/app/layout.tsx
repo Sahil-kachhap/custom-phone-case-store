aa47037cb17a34b6a02c9802cf28814e6b4f8d4c
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-07 19:13:22 +0530

    chore: changed font to poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ['latin'], weight: "400" });
 
 export const metadata: Metadata = {
   title: "CaseCanvas",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Navbar />
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex flex-col flex-1 h-full">
