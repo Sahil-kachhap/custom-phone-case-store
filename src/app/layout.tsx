d5359afc12ba8511f1066bb72ec7e56220f817f9
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-26 10:45:30 +0530

    chore: pass clerk publishable key in clerkprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,13 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Provider";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const poppins = Poppins({ subsets: ['latin'], weight: "400" });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "CaseCanvas - custom high-quality phone cases",
   description: "Get your custom high-quality phone cases in seconds",
-  
 };
 
 export default function RootLayout({
@@ -21,19 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={poppins.className}>
-        <Navbar />
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div className="flex flex-col flex-1 h-full">
-             <Providers>{children}</Providers>
+    <ClerkProvider publishableKey={process.env.CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={poppins.className}>
+          <Navbar />
+          <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+            <div className="flex flex-col flex-1 h-full">
+              <Providers>{children}</Providers>
             </div>
-          <Footer />
-        </main>
-        <Toaster />
-      </body>
-    </html>
+            <Footer />
+          </main>
+          <Toaster />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
